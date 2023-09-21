18c3ba336f263b70c448de6e99b3109362200195
Kasetty Venkata Naga Rohith <67185804+VenkataNagaRohith@users.noreply.github.com>
2023-09-21 11:03:30 +0530

    Delete course-service directory

--- course-service/src/main/java/com/training/courseservice/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.training.courseservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.training.courseservice.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long>{
-
-}
